836f78af930cf680417ad07ac8307b4fdf5cb1ac
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-02-06 11:12:32 +0530

    ADDED:Test cases for coding cafe

--- src/test/java/com/bridgelabz/codingcafe/CodingCafeApiTest.java
+++ src/test/java/com/bridgelabz/codingcafe/CodingCafeApiTest.java
@@ -2,6 +2,10 @@ package com.bridgelabz.codingcafe;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,19 +13,62 @@ import org.junit.Test;
 
 public class CodingCafeApiTest {
 
+    private  String tokenValue="eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiUUEiLCJlbWFpbElkIjoidGVzdGluZ3Rlc3RkYXRhYmx6QGdtYWlsLmNvbSIsImlkIjoiNWRjZmRiYmRjNTgzNmIwMDA2OTc4YWFhIiwiZXhwIjoxNTgwOTYxNjY0fQ.8KaZq5aXgmxc4_M8SE-029gldj9sUMwslwR941TrYLE";
+    Response response;
+
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp()  {
         RestAssured.baseURI="https://cccampdash.incubation.bridgelabz.com";
+
     }
 
     @Test
     public void givenMethodForgetCampaignByCampaignShortname() {
-        Response response= RestAssured.given()
-                //.param("campaignShortName",12231)
+        response= RestAssured.given()
                 .when()
-                .get("/campaign/dashbaord/campaign/12231");
+                .get("/campaign/dashbaord/campaign/ABH");
         response.prettyPrint();
             int statusCode=response.getStatusCode();
         Assert.assertEquals(statusCode,200);
+    }
+
+    @Test
+    public void givenMethodForGetCount() {
+         response=RestAssured.given()
+                .header("token",tokenValue)
+                .when()
+                .get("/campaign/dashboard");
+        response.prettyPrint();
+        int statusCode=response.getStatusCode();
+        Assert.assertEquals(statusCode,200);
+
+    }
+
+    @Test
+    public void givenMethodForGetListOfCampianType() {
+        response=RestAssured.given()
+                .pathParam("campaignType","Registration")
+                .header("token",tokenValue)
+                .when()
+                .get("campaign/dashboard/Registration");
+        response.prettyPrint();
+        int statusCode=response.getStatusCode();
+        Assert.assertEquals(statusCode,200);
+
+    }
+
+    @Test
+    public void givenmethodForGetCampianShortname() throws ParseException {
+        response=RestAssured.given()
+                .header("token",tokenValue)
+                .when()
+                .get("/campaign/dashboard/campaign/shortname");
+         int statusCode=response.getStatusCode();
+        ResponseBody body=response.getBody();
+        JSONObject object =(JSONObject) new JSONParser().parse(body.prettyPrint());;
+         String message= (String) object.get("message");
+         Assert.assertEquals(statusCode,200);
+         Assert.assertEquals("Campaign retrieved successfully",message);
     }
 }
